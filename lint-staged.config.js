ffe715a1e99862db961a412ae80c826b3b145d84
JoaoMoraess <joaomorais.cirne@gmail.com>
2021-09-30 18:21:32 -0300

    change lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint', 'yarn test:staged'],
-  '**/*.ts?(x)': () => 'yarn build-types',
-  '*.json': ['prettier --write']
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint', 'yarn test:staged']
 }
